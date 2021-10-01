27512a96f1f1d38956e5c62fef71b4e7e1851098
richard <ra22597@gmail.com>
2021-10-01 23:39:45 +0100

    additional document/ API tests

--- rspace_client/tests/api_test.py
+++ rspace_client/tests/api_test.py
@@ -11,10 +11,16 @@ import rspace_client as cli
 import unittest
 import os
 import pytest
+import random
+import string
 
 
 RSPACE_URL_ENV = "RSPACE_URL"
 RSPACE_APIKEY_ENV = "RSPACE_API_KEY"
+
+def random_string(length):
+    letters = string.ascii_lowercase
+    return (''.join(random.choice(letters) for i in range(length)) )
 
 
 class ApiClientIntegrationTest(unittest.TestCase):
@@ -31,8 +37,27 @@ class ApiClientIntegrationTest(unittest.TestCase):
             or len(self.rspace_apikey) == 0
         ):
             pytest.skip("Skipping API test as URL/Key are not defined")
+        self.api = cli.ELNClient(self.rspace_url, self.rspace_apikey)
 
     def test_get_status(self):
-        api = cli.ELNClient(self.rspace_url, self.rspace_apikey)
-        resp = api.get_status()
+        resp = self.api.get_status()
         self.assertEqual("OK", resp["message"])
+
+    def test_get_documents(self):
+        resp = self.api.get_documents()
+        self.assertTrue(resp['totalHits'] > 0)
+        self.assertTrue(len(resp['documents']) > 0)
+        
+    def test_get_documents_by_id(self):
+        resp = self.api.get_documents()
+        first_id = resp['documents'][0]['id']
+        doc = self.api.get_document(first_id)
+        self.assertEqual(first_id, doc['id'])
+        
+    def test_create_document(self):
+        nameStr = random_string(10) 
+        tag_str= random_string(5)
+        resp = self.api.create_document(name=nameStr, tags=tag_str)
+        print(resp)
+        self.assertEqual(nameStr, resp['name'])
+        self.assertEqual(tag_str, resp['tags'])
