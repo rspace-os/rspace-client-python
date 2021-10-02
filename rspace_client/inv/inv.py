18e8e4cf4a3823eb6abb22a9c16d7b708b14d2ef
richard <ra22597@gmail.com>
2021-10-03 00:28:13 +0100

    set additional sample data

--- rspace_client/inv/inv.py
+++ rspace_client/inv/inv.py
@@ -1,5 +1,34 @@
+from enum import Enum
 from rspace_client.client_base import ClientBase
+from typing import Optional,Sequence,Any
 
+class ExtraFieldType(Enum):
+    TEXT = "text"
+    NUMBER= "number"
+
+class TemperatureUnit(Enum):
+    CELSIUS=8
+    KELVIN=9
+    
+        
+    
+class StorageTemperature:
+    degrees: float
+    units: TemperatureUnit
+    
+    def __init__(self, degrees: float, units: TemperatureUnit = TemperatureUnit.CELSIUS):
+        self.degrees = degrees
+        self.units = units
+        
+    def _toDict (self) -> dict:
+        return {'unitId': self.units.value,
+                'numericValue': self.degrees}
+        
+class ExtraField:
+    def __init__(self, name: str,
+                 fieldType: ExtraFieldType = ExtraFieldType.TEXT, 
+                 content: Any =""):
+        self.data = {'name': name, 'type': fieldType.value, 'content': content}
 
 class InventoryClient(ClientBase):
     API_VERSION="v1"
@@ -12,7 +41,11 @@ class InventoryClient(ClientBase):
         
         return "{}/api/inventory/{}".format(self.rspace_url, self.API_VERSION)
     
-    def create_sample(self, name: str =None, tags: str=None) -> dict :
+    def create_sample(self, name: Optional[str] = None,
+                      tags: Optional[str] = None,
+                      extra_fields: Optional[Sequence] = [],
+                      storage_temperature_min = None,
+                      storage_temperature_max = None) -> dict :
         """
         Creates a new sample with optional attributes
         """
@@ -21,6 +54,12 @@ class InventoryClient(ClientBase):
             data["name"] = name
         if tags is not None:
             data["tags"] = name
+        if extra_fields is not None:
+            data['extraFields'] = [ ef.data for ef in extra_fields]
+        if storage_temperature_min is not None:
+           data["storageTempMin"]=storage_temperature_min._toDict()
+        if storage_temperature_max is not None:
+           data["storageTempMax"]=storage_temperature_max._toDict()
         return self.retrieve_api_results(
             self._get_api_url() + "/samples", request_type="POST", params=data
         )
--- rspace_client/tests/invapi_test.py
+++ rspace_client/tests/invapi_test.py
@@ -18,5 +18,18 @@ class InventoryApiTest(base.BaseApiTest):
     def test_create_sample(self):
         sample_name = base.random_string(5)
         sample_tags = base.random_string(4)
-        sample  = self.invapi.create_sample(name=sample_name, tags=sample_tags)
+        ef1 = cli.ExtraField("f1", cli.ExtraFieldType.TEXT, "hello")
+        ef2 = cli.ExtraField("f2", cli.ExtraFieldType.NUMBER, 123)
+
+        minTemp = cli.StorageTemperature(1, cli.TemperatureUnit.KELVIN)
+        maxTemp = cli.StorageTemperature(4, cli.TemperatureUnit.KELVIN)
+
+        sample  = self.invapi.create_sample(name=sample_name,
+                                            tags=sample_tags,
+                                            extra_fields=[ef1,ef2],
+                                            storage_temperature_min=minTemp,
+                                            storage_temperature_max=maxTemp,
+                                            )
         self.assertEqual(sample_name, sample['name'])
+        self.assertEqual(2, len(sample['extraFields']))
+        self.assertEqual(4, sample['storageTempMax']['numericValue'])
