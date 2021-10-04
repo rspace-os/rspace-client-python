768b00f38dbaa1f285f31bfa2c82f653f94184e8
richard <ra22597@gmail.com>
2021-10-04 19:22:20 +0100

    tests for quantity unit

--- rspace_client/inv/quantity_unit.py
+++ rspace_client/inv/quantity_unit.py
@@ -8,6 +8,9 @@ Created on Sun Oct  3 11:11:05 2021
 
 
 class QuantityUnit:
+    """
+    Static data from api/v1/units definitions
+    """
     data = [
         {"id": 1, "label": "items", "category": "dimensionless", "description": ""},
         {"id": 2, "label": "µl", "category": "volume", "description": ""},
@@ -16,7 +19,7 @@ class QuantityUnit:
         {"id": 5, "label": "µg", "category": "mass", "description": ""},
         {"id": 6, "label": "mg", "category": "mass", "description": ""},
         {"id": 7, "label": "g", "category": "mass", "description": ""},
-        {"id": 8, "label": "℃", "category": "temperature", "description": ""},
+        {"id": 8, "label": "C", "category": "temperature", "description": ""},
         {"id": 9, "label": "K", "category": "temperature", "description": ""},
         {"id": 10, "label": "F", "category": "temperature", "description": ""},
         {"id": 11, "label": "nmol/l", "category": "molarity", "description": ""},
@@ -29,5 +32,37 @@ class QuantityUnit:
     ]
 
     @staticmethod
-    def of(label):
-        return [x for x in QuantityUnit.data if x["label"] == label][0]
+    def of(label: str) ->dict :
+        """
+    
+        Parameters
+        ----------
+        label : str
+            String repersentation of a unit.
+
+        Raises
+        ------
+        ValueError
+            if no unit definition exists for .
+
+        Returns
+        -------
+        dict 
+            information about the unit definition.
+        """
+        units = QuantityUnit()._find_label(label)
+        if len(units) ==0:
+            raise ValueError(f"{label} not found in  unit data")
+        return units[0]
+    
+    @staticmethod
+    def is_supported_unit(label : str) -> bool :
+        return len(QuantityUnit._find_label(label)) > 0
+    
+    @staticmethod
+    def _find_label( label):
+         return  [x for x in QuantityUnit.data if x["label"] == label  ]      
+
+    @staticmethod
+    def unit_labels() -> tuple:
+        return [x['label'] for x in QuantityUnit.data]
