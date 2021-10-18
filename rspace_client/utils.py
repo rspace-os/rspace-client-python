4b1cd9a3cfd920b81de33d88816dd9254a07a8c2
richard <ra22597@gmail.com>
2021-10-18 20:27:44 +0100

    simple create-sample example script

--- rspace_client/utils.py
+++ rspace_client/utils.py
@@ -1,11 +1,9 @@
 import argparse
 import rspace_client.eln.eln as eln
+import rspace_client.inv.inv as inv
 
 
-def createELNClient():
-    """
-    Parses command line arguments: server, apiKey
-    """
+def _parse_args():
     parser = argparse.ArgumentParser()
     parser.add_argument(
         "server",
@@ -16,6 +14,20 @@ def createELNClient():
         "apiKey", help="RSpace API key can be found on 'My Profile'", type=str
     )
     args = parser.parse_args()
+    return args
 
+def createELNClient():
+    """
+    Parses command line arguments: server, apiKey
+    """ 
+    args = _parse_args()
     client = eln.ELNClient(args.server, args.apiKey)
+    return client
+
+def createInventoryClient():
+    """
+    Parses command line arguments: server, apiKey
+    """ 
+    args = _parse_args()
+    client = inv.InventoryClient(args.server, args.apiKey)
     return client
