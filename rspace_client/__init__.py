99fe618b59cb20543b4e786b7b2b3025f3c6a57f
richard <ra22597@gmail.com>
2022-02-19 11:07:59 +0000

    docs

--- rspace_client/__init__.py
+++ rspace_client/__init__.py
@@ -1,3 +1,6 @@
+"""
+ RSpace API client for interacting with RSpace ELN and Inventory
+"""
 from .eln.eln import ELNClient
 from .inv.inv import InventoryClient
 from .eln.advanced_query_builder import AdvancedQueryBuilder
